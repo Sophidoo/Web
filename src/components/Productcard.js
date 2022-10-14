1534cf274aa40af18544a4352199715d0ca26785
Sophia Okosodo <sophieokosodo@gmail.com>
2022-10-14 12:13:55 +0100

    home page responsive

--- src/components/Productcard.js
+++ src/components/Productcard.js
@@ -1,15 +1,16 @@
-const Productcard = (props) =>{
 
+const Productcard = ({data}) =>{
+
+        const {image, name, price} = data
     return <>
-
         <div className="card">
-            <img src={props.pic} alt="" />
+            <img src={image} alt="" />
             <span className="material-symbols-outlined cart" >
                 shopping_cart
             </span>
             <div className="productname">
-                <h3>{props.name}</h3>
-                <p>{props.price}</p>
+                <h3>{name}</h3>
+                <p>{price}</p>
             </div>
         </div>
 
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        image: 'http://cdn.shopify.com/s/files/1/0219/7173/0504/products/02.09.21-230.jpg?v=1631167063',
+        name: "Pink Hoodie",
+        price: "$50.00"
+    },
+    {
+        id: 2,
+        image: 'https://i.pinimg.com/originals/2e/9e/59/2e9e597383e356ca67b139eaa430038e.jpg',
+        name: "Corporate Gown",
+        price: "$400.00"
+    },
+    {
+        id: 3,
+        image: 'https://external-preview.redd.it/Id_RrlwGNFR00Xxh16vQu9iwBrASAOGOeOigb047kZA.jpg?auto=webp&s=aa287809805c55ad9684ada7fb500c7eb17115b6',
+        name: "Baggy Jeans",
+        price: "$100.00"
+    },
+    {
+        id: 4,
+        image: 'https://glorytrends.com/wp-content/uploads/2021/10/mens-suits-2022.jpg',
+        name: "Men's Suit",
+        price: "$1000.00"
+    }
+]
+
+export default data
